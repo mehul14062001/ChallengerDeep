5d64a7c8bf9b59d25b2047fc01ec9ebdf37e5573
mehul14062001 <77636118+mehul14062001@users.noreply.github.com>
2025-01-27 23:12:31 +0530

    Added DataFrame techniques

--- ChallengerDeep.py
+++ ChallengerDeep.py
@@ -1,3 +1,34 @@
+import os
+import threading
+import time
+import multiprocessing
+import sys
+import subprocess
+
+
+#####################
+## PACKAGE MANAGER ##
+#####################
+
+class PackageManager:
+    def __init__(self,package_name):
+        self.package_name=package_name
+    def is_package_installed(self):
+        try:
+            subprocess.check_call([sys.executable,"-m","pip","show",self.package_name],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+            return True
+        except subprocess.CalledProcessError:
+            return False
+    def install_package(self):
+        try:
+            subprocess.check_call(f'"{sys.executable}" -m pip install "{self.package_name}"',shell=True,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+        except subprocess.CalledProcessError as e:
+            pass
+    def check_and_install(self):
+        if not self.is_package_installed():    
+            self.install_package()
+
+
 ############
 ## BITSET ##
 ############
@@ -260,3 +291,330 @@ class PriorityQueue:
         self.heap[i],self.heap[j]=self.heap[j],self.heap[i]
     def __str__(self):
         return "Priority Queue (Min-Heap):"+str(self.heap)
+    
+
+########################
+## CUSTOM FILE THREAD ##
+########################
+
+class CustomFileThread(object):
+    @staticmethod
+    def process_chunk(chunk,start_index,result_queue,delimiter=','):
+        for i,chunk_line in enumerate(chunk):
+            fields=chunk_line.strip().split(delimiter)
+            result_queue.enqueue((start_index+i,fields))
+    @staticmethod
+    def read_file(file_path,num_threads=None,delimiter=',',datatypes=None):
+        result_queue=PriorityQueue()
+        threads=[]
+        if num_threads==None:
+            num_threads=os.cpu_count()
+        with open(file_path,"r") as file:
+            header=next(file).strip().split(delimiter)
+            lines=file.readlines()
+            chunk_size=len(lines)//num_threads
+            for i in range(0,len(lines),chunk_size):
+                chunk=lines[i:i+chunk_size]
+                thread=threading.Thread(target=CustomFileThread.process_chunk,args=(chunk,i,result_queue,delimiter))
+                threads.append(thread)
+                thread.start()
+            for thread in threads:
+                thread.join()
+        result=[[i.replace("\"","").replace("'","") for i in header]]
+        while not result_queue.is_empty():
+            _,line=result_queue.dequeue()
+            if datatypes!=None:
+                for d,datatype in enumerate(datatypes):
+                    if datatype=='int':
+                        if len(line[d])>0:
+                            line[d]=int(line[d].replace("\"","").replace("'",""))
+                    elif datatype=='float':
+                        if len(line[d])>0:
+                            line[d]=float(line[d].replace("\"","").replace("'",""))
+                    elif datatype=='bool':
+                        if len(line[d])>0:
+                            line[d]=bool(line[d].replace("\"","").replace("'",""))
+                    else:
+                        line[d]=str(line[d].replace("\"","").replace("'",""))
+            result.append(line)
+        return result
+
+
+#############################
+## CUSTOM DATAFRAME THREAD ##
+#############################
+
+class CustomDataFrameThread(object):
+    def __init__(self,data,column_names,num_threads=None):
+        self.header=column_names
+        data=data[1:]
+        self.df=[data[i] for i,_ in enumerate(data)]
+        if 'index' not in column_names:
+            self.df=[[i]+data[i] for i,_ in enumerate(data)]
+            self.header=['index']+column_names
+        self.num_threads=os.cpu_count() if num_threads==None else num_threads
+    def head_dataframe(self,n):
+        return [self.header]+self.df[:n]
+    def tail_dataframe(self,n):
+        return [self.header]+self.df[-n:]
+    def filter_dataframe_chunks(self,chunk,column_indices,conditions,values,result_queue):
+        for _,chunk_row in enumerate(chunk):
+            filtered_chunk_row=[chunk_row[j] for j in column_indices]
+            eligible_row=True
+            if conditions!=None and values!=None:
+                for j,c in enumerate(conditions):
+                    if c=='=':
+                        if filtered_chunk_row[j+1]!=values[j]:
+                            eligible_row=False
+                            break
+                    if c=='>':
+                        if filtered_chunk_row[j+1]<=values[j]:
+                            eligible_row=False
+                            break
+                    if c=='<':
+                        if filtered_chunk_row[j+1]>=values[j]:
+                            eligible_row=False
+                            break
+                    if c=='>=':
+                        if filtered_chunk_row[j+1]<values[j]:
+                            eligible_row=False
+                            break
+                    if c=='<=':
+                        if filtered_chunk_row[j+1]>values[j]:
+                            eligible_row=False
+                            break
+                    if c=='!=':
+                        if filtered_chunk_row[j+1]==values[j]:
+                            eligible_row=False
+                            break    
+            if eligible_row==True: 
+                result_queue.enqueue((filtered_chunk_row))
+    def filter_dataframe(self,columns,conditions=None,values=None):
+        if 'index' not in columns:
+            columns=['index']+columns
+        result_queue=PriorityQueue()
+        threads=[]
+        column_indices=[self.header.index(i) for i in columns]
+        chunk_size=len(self.df)//self.num_threads
+        for i in range(0,len(self.df),chunk_size):
+            chunk=self.df[i:i+chunk_size]
+            thread=threading.Thread(target=self.filter_dataframe_chunks,args=(chunk,column_indices,conditions,values,result_queue))
+            threads.append(thread)
+            thread.start()
+        for thread in threads:
+            thread.join()
+        result=[]
+        while not result_queue.is_empty():
+            chunk_row=result_queue.dequeue()
+            result.append(chunk_row)
+        result=CustomDataFrameThread(data=result,column_names=columns)
+        return result
+
+
+#########################
+## CUSTOM FILE PROCESS ##
+#########################
+
+class CustomFileProcess(object):
+    @staticmethod
+    def process_chunk(chunk,start_index,result_queue,delimiter=','):
+        for i,chunk_line in enumerate(chunk):
+            fields=chunk_line.strip().split(delimiter)
+            result_queue.put((start_index+i,fields))
+    @staticmethod
+    def read_file(file_path,num_processes=None,delimiter=',',datatypes=None):
+        manager=multiprocessing.Manager()
+        result_queue=manager.Queue()
+        processes=[]
+        num_processes=num_processes or os.cpu_count()
+        with open(file_path,"r") as file:
+            header=next(file).strip().split(delimiter)
+            lines=file.readlines()
+            chunk_size=len(lines)//num_processes
+            for i in range(0,len(lines),chunk_size):
+                chunk=lines[i:i+chunk_size]
+                process=multiprocessing.Process(target=CustomFileProcess.process_chunk,args=(chunk,i,result_queue,delimiter))
+                processes.append(process)
+                process.start()
+            for process in processes:
+                process.join()
+        result=[[i.replace("\"","").replace("'","") for i in header]]
+        while not result_queue.empty():
+            _,line=result_queue.get()
+            if datatypes is not None:
+                for d,datatype in enumerate(datatypes):
+                    if datatype=='int':
+                        if len(line[d])>0:
+                            line[d]=int(line[d].replace("\"","").replace("'",""))
+                    elif datatype=='float':
+                        if len(line[d])>0:
+                            line[d]=float(line[d].replace("\"","").replace("'",""))
+                    elif datatype=='bool':
+                        if len(line[d])>0:
+                            line[d]=bool(line[d].replace("\"","").replace("'",""))
+                    else:
+                        line[d]=str(line[d].replace("\"","").replace("'",""))
+            result.append(line)
+        return result
+
+
+##############################
+## CUSTOM DATAFRAME PROCESS ##
+##############################
+
+class CustomDataFrameProcess(object):
+    def __init__(self,data,column_names,num_processes=None):
+        self.header=column_names
+        data=data[1:]
+        self.df=[data[i] for i,_ in enumerate(data)]
+        if 'index' not in column_names:
+            self.df=[[i]+data[i] for i,_ in enumerate(data)]
+            self.header=['index']+column_names
+        self.num_processes=os.cpu_count() if num_processes is None else num_processes
+    def head_dataframe(self,n):
+        return [self.header]+self.df[:n]
+    def tail_dataframe(self,n):
+        return [self.header]+self.df[-n:]
+    @staticmethod
+    def filter_dataframe_chunks(chunk,column_indices,conditions,values,result_queue):
+        for _,chunk_row in enumerate(chunk):
+            filtered_chunk_row=[chunk_row[j] for j in column_indices]
+            eligible_row=True
+            if conditions is not None and values is not None:
+                for j,c in enumerate(conditions):
+                    if c=='=':
+                        if filtered_chunk_row[j+1] !=values[j]:
+                            eligible_row=False
+                            break
+                    if c=='>':
+                        if filtered_chunk_row[j+1] <=values[j]:
+                            eligible_row=False
+                            break
+                    if c=='<':
+                        if filtered_chunk_row[j+1]>=values[j]:
+                            eligible_row=False
+                            break
+                    if c=='>=':
+                        if filtered_chunk_row[j+1] < values[j]:
+                            eligible_row=False
+                            break
+                    if c=='<=':
+                        if filtered_chunk_row[j+1]>values[j]:
+                            eligible_row=False
+                            break
+                    if c=='!=':
+                        if filtered_chunk_row[j+1]==values[j]:
+                            eligible_row=False
+                            break
+            if eligible_row:
+                result_queue.put(filtered_chunk_row)
+    def filter_dataframe(self,columns,conditions=None,values=None):
+        if 'index' not in columns:
+            columns=['index']+columns
+        manager=multiprocessing.Manager()
+        result_queue=manager.Queue()
+        processes=[]
+        column_indices=[self.header.index(i) for i in columns]
+        chunk_size=len(self.df)//self.num_processes
+        for i in range(0,len(self.df),chunk_size):
+            chunk=self.df[i:i+chunk_size]
+            process=multiprocessing.Process(target=self.filter_dataframe_chunks,args=(chunk,column_indices,conditions,values,result_queue))
+            processes.append(process)
+            process.start()
+        for process in processes:
+            process.join()
+        result=[]
+        while not result_queue.empty():
+            result.append(result_queue.get())
+        result=CustomDataFrameProcess(data=result,column_names=columns)
+        return result
+
+
+#################
+## FILE PANDAS ##
+#################
+
+class FilePandas(object):
+    @staticmethod
+    def read_file(file_path):
+        package_manager=PackageManager(package_name='pandas')
+        package_manager.install_package()
+        import pandas as pd
+        df=pd.read_csv(file_path)
+        return df
+
+
+######################
+## DATAFRAME PANDAS ##
+######################
+
+class DataFramePandas(object):
+    def __init__(self,data):
+        self.header=data.columns.tolist()
+        self.df=data
+    def head_dataframe(self,n):
+        return self.df.head(n=n)
+    def tail_dataframe(self,n):
+        return self.df.tail(n=n)
+    def filter_dataframe(self,columns,conditions=None,values=None):
+        filtered_df=self.df
+        if conditions is not None and values is not None:
+            for j,c in enumerate(conditions):
+                if c=='=':
+                    filtered_df=filtered_df[filtered_df[columns[j]]==values[j]]
+                if c=='>':
+                    filtered_df=filtered_df[filtered_df[columns[j]]>values[j]]
+                if c=='<':
+                    filtered_df=filtered_df[filtered_df[columns[j]]<values[j]]
+                if c=='>=':
+                    filtered_df=filtered_df[filtered_df[columns[j]]>=values[j]]
+                if c=='<=':
+                    filtered_df=filtered_df[filtered_df[columns[j]]<=values[j]]
+                if c=='!=':
+                    filtered_df=filtered_df[filtered_df[columns[j]]!=values[j]]
+        return DataFramePandas(data=filtered_df[columns])
+    
+
+###############
+## FILE DASK ##
+###############
+
+class FileDask(object):
+    @staticmethod
+    def read_file(file_path):
+        package_manager=PackageManager(package_name='"dask[distributed,dataframe]"')
+        package_manager.install_package()
+        import dask.dataframe as dd
+        df=dd.read_csv(file_path)
+        return df
+
+
+####################
+## DATAFRAME DASK ##
+####################
+
+class DataFrameDask(object):
+    def __init__(self,data):
+        self.header=data.columns.tolist()
+        self.df=data
+    def head_dataframe(self,n):
+        return self.df.head(n=n)
+    def tail_dataframe(self,n):
+        return self.df.tail(n=n)
+    def filter_dataframe(self,columns,conditions=None,values=None):
+        filtered_df=self.df
+        if conditions is not None and values is not None:
+            for j,c in enumerate(conditions):
+                if c=='=':
+                    filtered_df=filtered_df[filtered_df[columns[j]]==values[j]]
+                if c=='>':
+                    filtered_df=filtered_df[filtered_df[columns[j]]>values[j]]
+                if c=='<':
+                    filtered_df=filtered_df[filtered_df[columns[j]]<values[j]]
+                if c=='>=':
+                    filtered_df=filtered_df[filtered_df[columns[j]]>=values[j]]
+                if c=='<=':
+                    filtered_df=filtered_df[filtered_df[columns[j]]<=values[j]]
+                if c=='!=':
+                    filtered_df=filtered_df[filtered_df[columns[j]]!=values[j]]
+        return DataFrameDask(data=filtered_df[columns])
